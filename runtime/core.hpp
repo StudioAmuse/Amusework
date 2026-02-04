a1d4820ff57dec3f5e04117b0ad4c4f32c51986d
ASOBIBA <86977637+LHBM04@users.noreply.github.com>
2026-02-04 11:34:05 +0900

    Move macros.hpp & types.hpp into ./misc
    - macro.hpp, types.hpp를 ./misc 폴더로 이동.

---
+++ runtime/core.hpp
@@ -0,0 +1,7 @@
+#ifndef AMUSE_INCLUDE_CORE_HPP
+#define AMUSE_INCLUDE_CORE_HPP
+
+#include "core/include/misc/macros.hpp"
+#include "core/include/misc/types.hpp"
+
+#endif // !AMUSE_INCLUDE_CORE_HPP
