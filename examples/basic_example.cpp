6bc108d13863a1915ef6d3f9dc5d4f2f43771511
ASOBIBA <86977637+LHBM04@users.noreply.github.com>
2026-02-04 11:44:31 +0900

    Delete runtime & examples skeleton
    - 임시로 작성해둔 스켈레톤 코드 삭제.

--- examples/basic_example.cpp
+++ examples/basic_example.cpp
@@ -1,36 +1,4 @@
-#include <amuse/core.hpp>
-#include <amuse/graphics.hpp>
-
-#include <iostream>
-
-using namespace amuse;
-
-int main(int argc_, char* argv_[])
+int main(int, char**);
 {
-    if (!core::Init())
-    {
-        std::cerr << "Failed to initialize!" << std::endl;
-        return -1;
-    }
-
-    if (!graphics::Init())
-    {
-        std::cerr << "Failed to initialize!" << std::endl;
-        core::Shutdown();
-        return -1;
-    }
-
-    constexpr int frameCount = 10;
-    for (int count = 0; count < frameCount; count++)
-    {
-        std::cout << "Frame " << (count + 1) << "/" << frameCount << std::endl;
-
-        graphics::CleanUp();
-        graphics::Present();
-    }
-
-    graphics::Shutdown();
-    core::Shutdown();
-
     return 0;
 }
--- runtime/include/amuse.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef AMUSE_INCLUDE_AMUSE_HPP
-#define AMUSE_INCLUDE_AMUSE_HPP
-
-#include "amuse/core.hpp"
-#include "amuse/graphics.hpp"
-
-#endif  // AMUSE_INCLUDE_AMUSE_H
